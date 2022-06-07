dc73495c22eb31412322190ef336734a8f23c8ab
Saeed Gholami Shahbandi <2572507+saeedghsh@users.noreply.github.com>
2022-06-07 23:13:25 +0200

    Fix bugs in main.py
    This commit fixes 3 bugs/issues in the `main.py`:
    * typo in `numpy.arange`,
    * the argument count was used without being added to the `parser`,
    * avoid abbreviating `argument` to `arg`

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """This script """
 
-
 import argparse
 import numpy
 
@@ -12,13 +11,14 @@ def parse_arguments():
         description="Time Series Predictive Models",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
+    parser.add_argument("--count", type=int, required=True, help="Some input from user")
     return parser.parse_args()
 
 
 def main():
     """Using a main function to avoid global variables"""
-    args = parse_arguments()
-    print(numpy.arrange(args.count))
+    arguments = parse_arguments()
+    print(numpy.arange(arguments.count))
 
 
 if __name__ == "__main__":
